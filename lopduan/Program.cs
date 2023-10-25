93b010ded8040bc39aaa71d1e06e8077ee4a272c
Huy Hau <Huy Hau@MSI>
2023-10-25 08:50:16 +0700

    Add project files.

---
+++ lopduan/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lopduan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            lopduan lopduan = new lopduan();
+            Console.WriteLine("nhap so luong du an: ");
+            int n = Convert.ToInt16(Console.ReadLine());
+            lopduan[] danhsach = new lopduan[n];
+            for (int i = 0; i < n; i++)
+            {
+
+                Console.WriteLine("nhap thong tin du an thu: " + (i + 1));
+
+
+                danhsach[i] = new lopduan();
+                danhsach[i].nhap();
+                
+
+            }
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("thong tin du an thu " + (i + 1));
+                danhsach[i].xuat();
+                danhsach[i].tinhTD();
+            }
+            
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lopduan/lopduan.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lopduan
+{
+    internal class lopduan
+    {
+        public string tenduan;
+        public DateTime ngaybatdau;
+        public DateTime ngayketthuc;
+        public lopduan()
+        {
+
+        }
+        public lopduan(string tenduan, DateTime ngaybatdau, DateTime ngayhientai, DateTime ngayketthuc)
+        {
+            tenduan = tenduan;
+            ngaybatdau = ngaybatdau;
+            ngayketthuc = ngayketthuc;
+        }
+
+        public void nhap()
+        {
+            Console.WriteLine(" nhap ten du an: ");
+            tenduan = Console.ReadLine();
+            Console.WriteLine("nhap ngay bat dau: ");
+            ngaybatdau =  DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", null);
+            Console.WriteLine("nhap ngay ket thuc:");
+            ngayketthuc = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", null);
+        }
+        public void xuat()
+        {
+            Console.WriteLine("ten du an la: " + tenduan);
+            Console.WriteLine("ngay ket thuc la: " + ngaybatdau.ToString("dd/MM/yyyy"));
+            Console.WriteLine("ngay ket thuc la: " + ngayketthuc.ToString("dd/MM/yyyy"));
+        }
+       public void tinhTD()
+        {
+            DateTime today = DateTime.Today;
+            TimeSpan tiendo;
+            if (ngayketthuc > today)
+            {
+                tiendo = today - ngaybatdau;
+            }
+            else
+            {
+                tiendo = ngayketthuc  - ngaybatdau;
+            }
+            double totalDays = (ngayketthuc - ngaybatdau).TotalDays;
+            double completedDays = tiendo.TotalDays;
+            if (totalDays <= 0)
+            {
+                Console.WriteLine("Ngày kết thúc phải lớn hơn ngày bắt đầu.");
+            }
+            else
+            {
+
+                if (ngayketthuc > today)
+                {
+                    double progress = (completedDays / totalDays) * 100;
+                    string phantramtiendo = progress.ToString("F2");
+                    Console.WriteLine(" tien do du an la: " + completedDays + " va da hoan thanh "+ phantramtiendo+ "%");
+                    
+                }
+                else
+                {
+                    double progress = (completedDays / totalDays) * 100;
+                    string phantramtiendo = progress.ToString("F2");
+                    Console.WriteLine(" tien do du an la: " + totalDays + " va da hoan thanh " + phantramtiendo + "%");
+                }
+            }
+
+        }
+       
+    }
+
+        
+}
